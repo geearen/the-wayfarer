730137412502f9b1908627d5341c6c32f454a9f7
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-08-19 10:21:01 -0700

    refactored profile extension

--- app_account/models.py
+++ app_account/models.py
@@ -6,6 +6,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(Model):
-  user = OneToOneField(User, on_delete=CASCADE)
+  user = ForeignKey(User, on_delete=CASCADE, related_name='profiles')
   profile_img = CharField(max_length=500, default="https://m.media-amazon.com/images/I/61lcM38-tfL._AC_UL1500_.jpg")
   current_city = CharField(max_length=50, default="NA")
