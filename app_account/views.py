0e33ff8c6a9787c76fece474a374d26c88cb49f0
geearen <allengonzales018@gmail.com>
2021-08-19 15:59:26 -0700

    added signup form, refactor models and views

--- app_account/views.py
+++ app_account/views.py
@@ -1,9 +1,10 @@
+from app_account.forms import SignUpForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views import View
 
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from .models import User, Profile
@@ -11,6 +12,7 @@ from .models import User, Profile
 # Create your views here.
 
 class Signup(View):
+  '''
   model = User, Profile
   fields = ['first_name', 'last_name', 'email', 'current_city', 'profile_img']
   
@@ -29,4 +31,28 @@ class Signup(View):
     else:
       context = {"form": form}
       return render(request, "signup.html", context)
+  '''
+  def get(self, request):
+    form = SignUpForm()
+    context = {
+      "form":form
+    }
+    return render(request, "signup.html", context)
   
+  def post(request):
+    if request.method == 'POST':
+      form = SignUpForm(request.POST)
+      if form.is_valid():
+        user = form.save()
+        
+        # If you don’t call user.refresh_from_db(), when you try to access the user.profile, it will return None.
+        user.refresh_from_db()
+        user.profile.current_city = form.cleaned_data.get('current_city')
+        user.save()
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=user.username, password=raw_password)
+        login(request, user)
+        return redirect('signup')
+    else:
+      form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
