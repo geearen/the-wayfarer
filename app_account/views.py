38afcb2d11767514a2b683433f29677d454d58f9
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-08-19 10:52:42 -0700

    fixed project_wayfarer urls.py path

--- app_account/views.py
+++ app_account/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views import View
 
@@ -5,23 +6,27 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from .models import User, Profile
 
 # Create your views here.
 
-class Profile(View):
+class Signup(View):
+  model = User, Profile
+  fields = ['first_name', 'last_name', 'email', 'current_city', 'profile_img']
   
   def get(self, request):
     form = UserCreationForm()
+
     context = {"form": form}
-    return render(request, "registration/signup.html", context)
+    return render(request, "signup.html", context)
 
   def post(self, request):
     form = UserCreationForm(request.POST)
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect("artist_list")
+      return redirect("signup")
     else:
       context = {"form": form}
-      return render(request, "registration/signup.html", context)
+      return render(request, "signup.html", context)
   
