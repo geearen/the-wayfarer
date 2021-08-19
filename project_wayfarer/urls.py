fbe65a8ebcd05e47af6364f6dd548e815c339bde
jennifergoldberg <jagoldbe@gmail.com>
2021-08-19 18:50:24 -0400

    working on carousel for home.html

--- project_wayfarer/urls.py
+++ project_wayfarer/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include ('app_account.urls')),
     path('', include ('app_content.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
