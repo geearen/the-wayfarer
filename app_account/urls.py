50094912dbd841fcf3227d5c108a6a81d2a95075
TevennLewis <tevennlewis@gmail.com>
2021-08-19 18:15:04 -0700

    made profile.html and path started

--- app_account/urls.py
+++ app_account/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import Signup
+from .views import Signup, Profile
 
 urlpatterns = [
 	path('signup/', Signup.as_view(), name="signup"),
+	path('<int:pk>/', Profile.as_view(), name="profile"),
 ]
