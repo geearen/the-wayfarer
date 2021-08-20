35772ac80f248752ffb1708486b0e2bc2fc4d46a
TevennLewis <tevennlewis@gmail.com>
2021-08-19 19:40:13 -0700

    completed profile detail route and basic html

--- app_account/urls.py
+++ app_account/urls.py
@@ -1,8 +1,8 @@
 
 from django.urls import path
-from .views import Signup, Profile
+from .views import Signup, ProfileDetail
 
 urlpatterns = [
 	path('signup/', Signup.as_view(), name="signup"),
-	path('<int:pk>/', Profile.as_view(), name="profile"),
+	path('<int:pk>/', ProfileDetail.as_view(), name="profile_detail"),
 ]
