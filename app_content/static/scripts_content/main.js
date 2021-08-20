241df4bc22ba8130d3eaf20d2abac193d9f7b91a
jennifergoldberg <jagoldbe@gmail.com>
2021-08-20 13:57:20 -0400

    added logo

--- app_content/static/scripts_content/main.js
+++ app_content/static/scripts_content/main.js
@@ -34,39 +34,39 @@ const caroselImages = [
 
 
 
-// const carouselIndex = 1;
-// altCarousel(carouselIndex);
+const carouselIndex = 1;
+altCarousel(carouselIndex);
 
-// // Next/previous controls
-// function alternate(n) {
-//   altCarousel(carouselIndex += n).on();
-// }
+// Next/previous controls
+function alternate(n) {
+  altCarousel(carouselIndex += n).on();
+}
 
 
-// function altCarousel(n) {
-//   let i = 1;
-//   const $image = $(".carousel_images, img");
+function altCarousel(n) {
+  let i = 1;
+  const image = $(".carousel_images");
   
-//   if (n > $image.length) {carouselIndex = 1}
-//   if (n < 1) {carouselIndex = $image.length}
-//   for (i = 0; i < $image.length; i++) {
-//     $image[i].css("display" = "none");
-//   }
-
-//   $image[carouselIndex-1].css("display" = "block");
-// }
-
-
-let carouselIndex = 0;
-altCarousel();
-
-function altCarousel() {
-  let i;
-  let slides = document.getElementsByClassName("carousel_images");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  if (n > image.length) {carouselIndex = 1}
+  if (n < 1) {carouselIndex = image.length}
+  for (i = 0; i < image.length; i++) {
+    image[i].css("display" = "none");
   }
-  carouselIndex++;
-  if (carouselIndex > slides.length) {carouselIndex = 1}
-  slides[carouselIndex-1].style.display = "block";
-  setTimeout(altCarousel, 2000)}; 
+
+  image[carouselIndex-1].css("display" = "block");
+}
+
+
+// let carouselIndex = 0;
+// altCarousel();
+
+// function altCarousel() {
+//   let i;
+//   let slides = document.getElementsByClassName("carousel_images");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   carouselIndex++;
+//   if (carouselIndex > slides.length) {carouselIndex = 1}
+//   slides[carouselIndex-1].style.display = "block";
+//   setTimeout(altCarousel, 2000)}; 
