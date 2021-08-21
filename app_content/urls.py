a4c27baf67cd4fc56803f8a8be6d3a9bccf7f5e6
jennifergoldberg <jagoldbe@gmail.com>
2021-08-21 15:26:35 -0400

    post detail page

--- app_content/urls.py
+++ app_content/urls.py
@@ -1,8 +1,8 @@
 from os import name
 from django.urls import path
-from .views import Home, PostShow
+from .views import Home, PostDetail
 
 urlpatterns = [
 	path('', Home.as_view(), name="home"),
-	path('posts/<int:pk>/', PostShow.as_view(), name="post_show"),
+	path('posts/<int:pk>/', PostDetail.as_view(), name="post_detail"),
 ]
